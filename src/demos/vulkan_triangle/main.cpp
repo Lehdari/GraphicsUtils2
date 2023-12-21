bb83a6b6e1a15ea4aa9ad11b66def746e478d631
Lehdari <miika.lehtimaki@gmail.com>
2023-12-21 20:34:54 +0200

    Add initial CMake config

---
+++ src/demos/vulkan_triangle/main.cpp
@@ -0,0 +1,19 @@
+//
+// Project: GraphicsUtils2
+// File: main.cpp
+//
+// Copyright (c) 2023 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include <cstdio>
+
+
+int main(void)
+{
+    printf("helou!\n");
+
+    return 0;
+}
