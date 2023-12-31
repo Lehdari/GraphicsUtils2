09f3d4d5c28c2910a43921c1e8d5ba20173d087c
Lehdari <miika.lehtimaki@gmail.com>
2023-12-31 03:55:18 +0200

    Update test_windows

--- src/tests/test_windows/main.cpp
+++ src/tests/test_windows/main.cpp
@@ -12,53 +12,106 @@
 #include <gu2_os/Window.hpp>
 
 
-class Window1 : public gu2::Window<Window1> {
+// Window closed by window close event
+GU2_WINDOW(WindowCloseWindow) {
 public:
-    Window1(const gu2::WindowSettings& settings) :
-        Window<Window1> (settings)
-    {
-    }
-
-    ~Window1() {}
+    WindowCloseWindow(const gu2::WindowSettings& settings) :
+        Window<WindowCloseWindow> (settings)
+    {}
 
     void handleEvent(const gu2::Event& event)
     {
-        switch (event.type) {
-            case gu2::Event::WINDOW:
-                switch (event.window.event) {
-                    case gu2::WindowEventID::CLOSE: close(); return;
-                }
-                break;
-            default: break;
+        if (event.type == gu2::Event::WINDOW &&
+            event.window.event == gu2::WindowEventID::CLOSE) {
+            close();
         }
     }
 
-    void render()
+    void render() {}
+};
+
+// Window closed by escape key press
+GU2_WINDOW(EscapeKeyWindow) {
+public:
+    EscapeKeyWindow(const gu2::WindowSettings& settings) :
+        Window<EscapeKeyWindow> (settings)
+    {}
+
+    void handleEvent(const gu2::Event& event)
     {
-        // TODO
+        if (event.type == gu2::Event::KEY &&
+            event.key.state == gu2::KeyEventState::PRESSED &&
+            event.key.sym.scancode == gu2::ScanCode::ESCAPE) {
+            close();
+        }
     }
+
+    void render() {}
 };
 
 
 int main(void)
 {
     gu2::WindowSettings settings1;
-    settings1.name = "Little Window";
-    settings1.w = 640;
-    settings1.h = 480;
-    Window1 window1(settings1);
+    settings1.name = "Little WindowCloseWindow";
+    settings1.w = 300;
+    settings1.h = 200;
+    WindowCloseWindow window1(settings1);
 
     gu2::WindowSettings settings2;
-    settings2.name = "Big Window";
-    settings2.w = 800;
-    settings2.h = 600;
-    Window1 window2(settings2);
+    settings2.name = "Big WindowCloseWindow";
+    settings2.w = 400;
+    settings2.h = 300;
+    WindowCloseWindow window2(settings2);
+
+    gu2::WindowSettings settings3;
+    settings3.name = "Little EscapeKeyWindow";
+    settings3.w = 300;
+    settings3.h = 200;
+    EscapeKeyWindow window3(settings3);
+
+    gu2::WindowSettings settings4;
+    settings4.name = "Big EscapeKeyWindow";
+    settings4.w = 400;
+    settings4.h = 300;
+    EscapeKeyWindow window4(settings4);
 
     gu2::App::addWindow(&window1);
     gu2::App::addWindow(&window2);
+    gu2::App::addWindow(&window3);
+    gu2::App::addWindow(&window4);
 
-    // Main loop
-    while (gu2::App::update());
+#if GU2_BACKEND == GU2_BACKEND_SDL2
+    {
+        // Send SDL events to close the windows
+        SDL_Event event;
+        event.type = SDL_WINDOWEVENT;
+        event.window.event = SDL_WindowEventID::SDL_WINDOWEVENT_CLOSE;
+        event.window.windowID = window1.getId();
+        SDL_PushEvent(&event);
+        event.window.windowID = window2.getId();
+        SDL_PushEvent(&event);
+
+        event.type = SDL_KEYDOWN;
+        event.key.keysym.scancode = SDL_SCANCODE_ESCAPE;
+        event.window.windowID = window3.getId();
+        SDL_PushEvent(&event);
+        event.window.windowID = window4.getId();
+        SDL_PushEvent(&event);
+    }
+#elif GU2_BACKEND == GU2_BACKEND_GLFW
+    {
+        // GLFW uses callbacks, call them directly
+        gu2::App::windowCloseCallback<WindowCloseWindow>(window1.getId());
+        gu2::App::windowCloseCallback<WindowCloseWindow>(window2.getId());
+        gu2::App::keyCallback<EscapeKeyWindow>(window3.getId(), GLFW_KEY_ESCAPE, -1, GLFW_PRESS, 0);
+        gu2::App::keyCallback<EscapeKeyWindow>(window4.getId(), GLFW_KEY_ESCAPE, -1, GLFW_PRESS, 0);
+    }
+#endif
+
+    // Main loop, should return after 1 update
+    if (gu2::App::update())
+        return 1;
 
     return 0;
 }
