49574c98d6106553d2798eff06867b8b6d6c0679
Lehdari <miika.lehtimaki@gmail.com>
2024-01-03 20:47:43 +0200

    demos/vulkan_triangle: Add vulkan instance initialization

--- src/demos/vulkan_triangle/main.cpp
+++ src/demos/vulkan_triangle/main.cpp
@@ -11,16 +11,55 @@
 #include <gu2_os/App.hpp>
 #include <gu2_os/Window.hpp>
 
+#include <vulkan/vulkan.h>
 
-class MyWindow : public gu2::Window<MyWindow> {
+
+class VulkanWindow : public gu2::Window<VulkanWindow> {
 public:
-    MyWindow(const gu2::WindowSettings& settings) :
-        Window<MyWindow>    (settings),
-        _running            (true)
+    VulkanWindow(const gu2::WindowSettings& settings) :
+        Window<VulkanWindow>    (settings)
     {
+        initVulkan();
     }
 
-    ~MyWindow() {}
+    ~VulkanWindow()
+    {
+        vkDestroyInstance(_vulkanInstance, nullptr);
+    }
+
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "GraphicsUtils2 Vulkan Triangle Demo";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "GraphicsUtils2";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        auto requiredExtensions = gu2::getVulkanInstanceExtensions();
+
+        #ifdef VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        #endif
+
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &_vulkanInstance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create vulkan instance!");
+        }
+    }
 
     void handleEvent(const gu2::Event& event)
     {
@@ -32,15 +71,6 @@ public:
                 }
                 break;
             case gu2::Event::KEY:
-                switch (event.key.state) {
-                    case gu2::KeyEventState::PRESSED:
-                        printf("pressed, "); break;
-                    case gu2::KeyEventState::RELEASED:
-                        printf("released, "); break;
-                    case gu2::KeyEventState::REPEATED:
-                        printf("repeated, "); break;
-                }
-                printf("keycode: %c\n", event.key.sym.keycode);
                 switch (event.key.sym.scancode) {
                     case gu2::ScanCode::ESCAPE: close(); return;
                     default: break;
@@ -56,29 +86,28 @@ public:
     }
 
 private:
-    bool    _running;
+    VkInstance  _vulkanInstance;
 };
 
 
 int main(void)
 {
-    gu2::WindowSettings settings1;
-    settings1.name = "Little Window";
-    settings1.w = 640;
-    settings1.h = 480;
-    MyWindow window1(settings1);
+    gu2::WindowSettings settings;
+    settings.name = "Hello Vulkan Triangle!";
+    settings.w = 800;
+    settings.h = 600;
+    VulkanWindow window(settings);
 
-    gu2::WindowSettings settings2;
-    settings2.name = "Big Window";
-    settings2.w = 800;
-    settings2.h = 600;
-    MyWindow window2(settings2);
-
-    gu2::App::addWindow(&window1);
-    gu2::App::addWindow(&window2);
+    gu2::App::addWindow(&window);
 
     // Main loop
-    while (gu2::App::update());
+    try {
+        while (gu2::App::update());
+    }
+    catch (const std::exception& e) {
+        fprintf(stderr, "%s\n", e.what());
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
